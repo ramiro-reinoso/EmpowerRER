73a0347cc2dbb9b4a611b8d2f90f1e4bb9eaa1e7
Ramiro Reinoso <ramiro.reinoso@verizon.net>
2023-05-16 14:09:22 -0400

    Add project files.

---
+++ EmpowerDSM_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerDSM_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerDSM_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("2119378E-F049-46A7-866F-25E9AE749F24")]
+[assembly: AssemblyVersion("1.0.0.0")]
